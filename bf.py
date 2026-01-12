ade3f10c094b250afde5074462b3bc4303a1c630
Vaia Patta <vaia.patta@gmail.com>
2026-01-12 13:02:57 +0200

    began bitfieldise function

--- bf.py
+++ bf.py
@@ -1,5 +1,10 @@
 
 
+C_TYPE_TO_REPLACE = ['char', 'short', 'long']  # TODO others?
 
-def bitfieldise(filename_in, bitarr, dim1, dim2):
-    pass
+
+def bitfieldise(filename_in: str, bitarr: str, dim1: int , dim2: int) -> None:
+    nn1 = dim1 / 8
+    n1 = int(nn1)
+    if n1 == nn1:
+        n1 += 1
